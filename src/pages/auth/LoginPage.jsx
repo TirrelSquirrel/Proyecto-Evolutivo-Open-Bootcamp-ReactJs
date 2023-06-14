120b56e3104a8abea37dee9e7eed6f141911bd2b
BauteNonSinger <bautista19298@gmail.com>
2023-06-14 17:06:35 +0200

    Preparando vistas

---
+++ src/pages/auth/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginForm from '../../components/pure/forms/loginForm';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <LoginForm></LoginForm>
+        </div>
+    );
+}
+
+export default LoginPage;
---
+++ src/pages/auth/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegisterForm from '../../components/pure/forms/registerForm';
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <h1>Register Page</h1>
+            <RegisterForm></RegisterForm>
+        </div>
+    );
+}
+
+export default RegisterPage;
